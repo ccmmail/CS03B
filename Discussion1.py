94606be28676643107aeb38bca256c237dab10f1
ccmmail <ccmmail@gmail.com>
2025-01-09 09:52:20 -0800

    added logic to check that assigned ID is unique

--- Discussion1.py
+++ Discussion1.py
@@ -9,13 +9,23 @@ class Books(object):
     Getters raise an assertion error if argument is of incorrect type
 
     Args:
-        ID: string unique alphanumeric ID
+        ID: string ID. ValueException error if ID is not unique
         title: string title of the book
         pages: integer number of pages in the book
     """
     ID: str = field(default = "0000")
     title: str = field(default = "Unknown")
     pages: int = field(default = 0)
+
+    # Class level set to keep track of unique IDs
+    _unique_IDs = set()
+
+    def __post_init__(self):
+        """Check that ID is unique on instantiation."""
+        if self.ID in Books._unique_IDs and self.ID != "0000":
+            raise ValueError(f"ERROR: ID '{self.ID}' already exists.")
+        else:
+            Books._unique_IDs.add(self.ID)
 
     def __str__(self):
         """Return a string representation of self"""
@@ -34,15 +44,24 @@ class Books(object):
         try:
             assert type(ID) == str
         except AssertionError:
-            print("Error: ID must be a string.")
+            print("ERROR: ID must be a string.")
         else:
-            self.ID = ID
+            try:
+                assert ID not in Books._unique_IDs
+            except AssertionError:
+                print("ERROR: ID must be unique.")
+            else:
+            #   if ID in self._unique_IDs:
+            #        self._unique_IDs.remove(self.ID)
+                Books._unique_IDs.remove(self.ID)
+                self.ID = ID
+                Books._unique_IDs.add(ID)
 
     def set_title(self, title:str):
         try:
             assert type(title) == str
         except AssertionError:
-            print("Error: Title must be a string.")
+            print("ERROR: Title must be a string.")
         else:
             self.title = title
 
@@ -50,6 +69,7 @@ class Books(object):
         try:
             assert type(pages) == int
         except AssertionError:
-            print("Error: Pages must be an integer.")
+            print("ERROR: Pages must be an integer.")
         else:
             self.pages = pages
+
